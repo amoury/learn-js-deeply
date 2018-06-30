9e66e906fdbdeaa3f5c8bfa1e448a254181bab24
Amoury <ansar.memon@hotmail.com>
2018-06-30 11:28:51 +0400

    Additional Object Assign Exercises

--- 4_Objects/object-assign.js
+++ 4_Objects/object-assign.js
@@ -7,10 +7,12 @@ var latestId = 0,
         }
 
     },
-    post;
+    post,
+    comment;
 
 
 post = Object.assign({}, content, { id: getNewId() });
+comment = Object.assign({}, post, { id: getNewId(), postId: post.id });
 
 
 function getNewId() {
@@ -18,6 +20,41 @@ function getNewId() {
 }
 
 post.render();
-// console.log(content);
+comment.render();
 
-// console.log(content.isPrototypeOf(post));
+
+/* 
+Notes - Basically Object.assign creates a new copy of the copy and the new copy is totally independent of the original object. It is not a reference to the old object.
+*/
+
+// Proof
+
+var originalObj = {
+        id: 20,
+        name: "John Doe",
+        occupation: "Student"
+    },
+    duplicateObj;
+
+// This is how to create reference to OrginalObj
+duplicateObj = originalObj;
+
+console.log(duplicateObj);
+
+originalObj.country = "Germany";
+
+console.log(duplicateObj);
+
+duplicateObj.street = "New Street";
+
+console.log(originalObj);
+
+// This is how to create a copy of the object that is independent of the original
+
+duplicateObj = Object.assign({}, originalObj);
+console.log(duplicateObj);
+
+originalObj.university = "New University";
+console.log("Duplicate Object: ", duplicateObj); // Doesn't show University Property
+
+console.log("Original Object : ", originalObj);
